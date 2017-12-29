c77f964b952d31e45b149ae25d92f6556957678d
iroegbu <iroegbuogechi@gmail.com>
2017-12-29 15:30:37 +0100

    moah tests!

--- test/Unit/ClientTest.php
+++ test/Unit/ClientTest.php
@@ -3,8 +3,21 @@
 namespace Test\Unit\Credentials;
 
 use iroegbu\WebPay\Client;
+use iroegbu\WebPay\Credentials;
 
-class ClientTest
+class ClientTest extends \PHPUnit_Framework_TestCase
 {
+    public function createClient()
+    {
+        return new Client(new Credentials('hashkey'), 'development');
+    }
 
+    /**
+     * @covers iroegbu\WebPay\Client::__construct
+     */
+    public function testImplementsCorrectInterface()
+    {
+        $client = $this->createClient();
+        $this->assertInstanceOf('iroegbu\WebPay\Client', $client);
+    }
 }
--- test/Unit/CredentialsTest.php
+++ test/Unit/CredentialsTest.php
@@ -6,9 +6,20 @@ use iroegbu\WebPay\Credentials;
 
 class CredentialsTest extends \PHPUnit_Framework_TestCase
 {
+    private function createCredentials()
+    {
+        return new Credentials('hash');
+    }
+
     public function testExistence()
     {
-        $credentials = new Credentials('hash');
+        $credentials = $this->createCredentials();
+        $this->assertInstanceOf('iroegbu\WebPay\Credentials', $credentials);
+    }
+
+    public function testHash()
+    {
+        $credentials = $this->createCredentials();
         $this->assertSame('hash', $credentials->getHash());
     }
 }
